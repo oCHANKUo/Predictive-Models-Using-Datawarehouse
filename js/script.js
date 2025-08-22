234069f936495959654595259ec3ee52299eb131
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-08-22 09:42:13 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function predictSales() {
     const feature = document.getElementById("feature").value;
+
+    fetch("http://localhost:5000//predict_sales", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ feature})
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").innerText = "Prediction: " + data.prediction;
+    })
+
+    
 }
