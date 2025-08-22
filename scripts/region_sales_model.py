bebdfb01b9323dabd45c13d296fec2d53c59b3d9
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-08-22 08:18:11 +0530

    region sales init

---
+++ scripts/region_sales_model.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pyodbc
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+
+app = Flask(__name__)
+MODEL_FILE = "region_sales_model.pkl"
+
+def get_connection():
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=localhost\\MSSQLSERVER03;DATABASE=DataWarehouseClassic;UID=admin;PWD=admin"
+    )
+    return conn
+
+def fetch_data():
+    query = """
+    
+    """
+    conn = get_connection()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
